d4538ce35dd3ed3f6bfe14585ddf6f3db87d3c8f
Zander Work <zander@zanderwork.com>
2023-07-02 02:33:11 -0400

    yay pylint

--- filedrop/lib/filestore.py
+++ filedrop/lib/filestore.py
@@ -1,11 +1,18 @@
-"""
-Manager for an on-disk filestore
-"""
+import filedrop.lib.database as f_db
 
 DEFAULT_MAX_SIZE = 10 * 1024 * 1024 * 1024  # 10gb
 
 
 class Filestore:
-    def __init__(self, db, root_path: str, max_size: int = DEFAULT_MAX_SIZE):
+    """Manager for an on-disk filestore"""
+
+    def __init__(self, db: f_db.Database, root_path: str, max_size: int = DEFAULT_MAX_SIZE):
+        self._db = db
         self._root_path = root_path
         self._max_size = max_size
+
+    def a(self):
+        """a"""
+
+    def b(self):
+        """b"""
